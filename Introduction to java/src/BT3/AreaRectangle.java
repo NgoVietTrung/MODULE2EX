e14338b1757beff870d80231538c0a09378bfb84
NgoVietTrung <0611vietttrung@gmail.com>
2022-06-03 15:54:58 +0700

    add AreaRectangle

---
+++ Introduction to java/src/BT3/AreaRectangle.java
@@ -0,0 +1,17 @@
+package BT3;
+
+import java.util.Scanner;
+
+public class AreaRectangle {
+    public static void main(String[] args) {
+        float width;
+        float height;
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Nhap width");
+        width = scanner.nextFloat();
+        System.out.println("Nhap height");
+        height = scanner.nextFloat();
+        float area=width*height;
+        System.out.println("Dien tich la " + area);
+    }
+}
