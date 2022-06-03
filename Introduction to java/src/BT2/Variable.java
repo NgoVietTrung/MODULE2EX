6d5e78ffd3528ecaf2bc2da66df819e3f378d0ee
NgoVietTrung <0611vietttrung@gmail.com>
2022-06-03 16:43:45 +0700

    add Equation

--- Introduction to java/src/BT2/Variable.java
+++ Introduction to java/src/BT2/Variable.java
@@ -7,13 +7,12 @@ public class Variable {
         double d=20.5;
         boolean b=true;
         char c='a';
-        String s="Hà Nội";
+        String s="Ha Noi";
         System.out.println("i= "+i);
         System.out.println("f= "+f);
         System.out.println("d= "+d);
         System.out.println("b= "+b);
         System.out.println("c= "+c);
         System.out.println("s= "+s);
-
     }
 }
