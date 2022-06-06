d89278b811c67279e1589054595489ba58f1ecdd
NgoVietTrung <0611vietttrung@gmail.com>
2022-06-06 13:38:56 +0700

    add Bank

---
+++ LoopingJava/src/BT2/Bank.java
@@ -0,0 +1,22 @@
+package BT2;
+
+import java.util.Scanner;
+
+public class Bank {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("So tien gui ");
+        double money = scanner.nextDouble();
+        System.out.print("Lai suat ");
+        double laisuat = scanner.nextDouble();
+        System.out.print("So thang gui ");
+        int month = scanner.nextInt();
+        double tienlai = 0;
+        for (int i = 1; i < month; i++) {
+
+            tienlai += money * (laisuat / 100) / 12 * month;
+
+        }
+        System.out.printf("Tien lai %.3f\n ", tienlai);
+    }
+}
