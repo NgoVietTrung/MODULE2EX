cb122528321b1f1b1d6bdfb7606487410ef3aac1
NgoVietTrung <0611vietttrung@gmail.com>
2022-06-06 10:31:53 +0700

    add BMI

--- Introduction to java/src/BT6/SpecialYear.java
+++ Introduction to java/src/BT6/SpecialYear.java
@@ -4,13 +4,13 @@ import java.util.Scanner;
 
 public class SpecialYear {
     public static void main(String[] args) {
-        Scanner scanner=new Scanner(System.in);
+        Scanner scanner = new Scanner(System.in);
         System.out.println("Nhap nam");
-        int year=scanner.nextInt();
-                if((year%400==0)||(year%4==0&&year%100!=0)){
-                    System.out.println(year + " la nam nhuan");
-                }else {
-                    System.out.println(year + " khong phai la nam nhuan");
-                }
+        int year = scanner.nextInt();
+        if ((year % 400 == 0) || (year % 4 == 0 && year % 100 != 0)) {
+            System.out.println(year + " la nam nhuan");
+        } else {
+            System.out.println(year + " khong phai la nam nhuan");
+        }
     }
 }
