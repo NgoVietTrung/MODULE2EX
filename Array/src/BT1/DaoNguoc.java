4ed9149e43c0ee59e870f46f78ddd49c5adea028
NgoVietTrung <0611vietttrung@gmail.com>
2022-06-07 11:14:03 +0700

    add DaoNguoc

---
+++ Array/src/BT1/DaoNguoc.java
@@ -0,0 +1,28 @@
+package BT1;
+
+import javax.swing.*;
+import java.util.Scanner;
+
+public class DaoNguoc {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int[] numbers = new int[20];
+//        int[] reversenumbers=new int[20];
+        for (int i = 0; i < numbers.length; i++) {
+            System.out.print("Numbers[" + i + "]: ");
+            numbers[i] = scanner.nextInt();
+        }
+        for(int i=0;i< numbers.length/2;i++){
+            int temp=numbers[i];
+            numbers[i]= numbers[numbers.length-1-i];
+            numbers[numbers.length-1-i]= temp;
+        }
+        for (int i:numbers)
+            System.out.print(i+" ");
+    }
+}
+
+
+
+
+
