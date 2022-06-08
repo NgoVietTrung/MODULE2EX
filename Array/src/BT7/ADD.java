142c124ee6bcc8f9393b9620b19bd76a8f425b7c
NgoVietTrung <0611vietttrung@gmail.com>
2022-06-08 10:51:47 +0700

    update ADD

--- Array/src/BT7/ADD.java
+++ Array/src/BT7/ADD.java
@@ -5,20 +5,25 @@ import java.util.Scanner;
 public class ADD {
     public static void main(String[] args) {
         Scanner scanner=new Scanner(System.in);
-        System.out.println("Nhap do dai cua mang ");
-        int length=scanner.nextInt();
-        System.out.println("So can chen ");
+       int numbers[]={1,2,3,5,6};
+        System.out.print("So can chen ");
         int number=scanner.nextInt();
-        System.out.println("Vi tri can chen ");
+        System.out.print("Vi tri can chen ");
         int index=scanner.nextInt();
-        int[] numbers=new int[length];
-        if(index>=0&&index<length){
-            numbers[index]=number;
-        }else {
-            System.out.println("Khong chen duoc");
+        int numbers2[]=new int[numbers.length+1];
+        int j=0;
+        for(int i=0;i<index;i++){
+            numbers2[i]=numbers[i];
+            j++;
         }
-        for (int i:numbers){
-            System.out.print( i+" ");
+        numbers2[j]=number;
+       j++;
+        for (int i=index;i<numbers.length;i++){
+            numbers2[j]=numbers[i];
+            j++;
+        }
+        for (int i:numbers2){
+            System.out.println(i+" ");
         }
     }
 }
