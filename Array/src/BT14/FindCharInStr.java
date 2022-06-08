f2e1532245c7725e05f69d45e6800cd989484eb8
NgoVietTrung <0611vietttrung@gmail.com>
2022-06-08 09:11:01 +0700

    add FindCharInStr

---
+++ Array/src/BT14/FindCharInStr.java
@@ -0,0 +1,21 @@
+package BT14;
+
+import java.util.Scanner;
+
+public class FindCharInStr {
+    public static void main(String[] args) {
+        Scanner scanner=new Scanner(System.in);
+        System.out.print("Nhap chuoi ");
+        String str=scanner.nextLine();
+        System.out.print("Ky tu can tim ");
+        String kytu=scanner.nextLine();
+        String str2=str.toLowerCase();
+        int count=0;
+        for (int i=0;i<str.length();i++){
+            if(kytu.charAt(0)==str2.charAt(i)){
+                count++;
+            }
+        }
+        System.out.print("Ky tu "+kytu+" xuat hien "+count+" lan trong "+str);
+    }
+}
