5938fd5234c9c7d4cafc26fe87c9ccaced4336d2
NgoVietTrung <0611vietttrung@gmail.com>
2022-06-09 11:06:53 +0700

    update PrimeNum

--- LoopingJava/src/BT6/PrimeNum.java
+++ LoopingJava/src/BT6/PrimeNum.java
@@ -3,7 +3,7 @@ package BT6;
 public class PrimeNum {
     public static void main(String[] args) {
         int demnt = 0;
-        for (int i = 1; demnt < 20; i++) {
+        for (int i = 1; demnt <= 20; i++) {
             int demuoc = 0;
             for (int j = 1; j <= i; j++)
                 if (i % j == 0) {
