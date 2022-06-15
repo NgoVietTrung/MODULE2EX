5371411750340c13a1ab2ea351b8cb60f7058a0d
NgoVietTrung <0611vietttrung@gmail.com>
2022-06-15 14:32:39 +0700

    add BT3

--- OOP/src/BT3/Main.java
+++ OOP/src/BT3/Main.java
@@ -1,4 +1,12 @@
 package BT3;
 
 public class Main {
+    public static void main(String[] args) {
+        StopWatch stopWatch=new StopWatch();
+        stopWatch.end();
+        for (int i=1;i<1000000;i++){
+            stopWatch.start();
+            stopWatch.getElapseTime();
+        }
+    }
 }
--- OOP/src/BT3/StopWatch.java
+++ OOP/src/BT3/StopWatch.java
@@ -1,4 +1,28 @@
 package BT3;
 
+import java.time.LocalTime;
+
 public class StopWatch {
+    private LocalTime startTime;
+    private LocalTime endTime;
+
+    public LocalTime getStartTime() {
+        return startTime;
+    }
+
+    public LocalTime getEndTime() {
+        return endTime;
+    }
+    public void start(){
+        this.startTime=LocalTime.now();
+        getStartTime();
+    }
+    public void end(){
+        this.endTime=LocalTime.now();
+        getEndTime();
+    }
+    public void getElapseTime(){
+        int ElapseTime=startTime.toSecondOfDay()-endTime.toSecondOfDay();
+        System.out.println(ElapseTime);
+    }
 }
