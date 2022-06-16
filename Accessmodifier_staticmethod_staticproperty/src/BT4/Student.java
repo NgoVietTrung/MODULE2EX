d701c5e124dee67302511e405c9c181221f22b19
NgoVietTrung <0611vietttrung@gmail.com>
2022-06-16 11:43:19 +0700

    add BT4

---
+++ Accessmodifier_staticmethod_staticproperty/src/BT4/Student.java
@@ -0,0 +1,19 @@
+package BT4;
+
+public class Student {
+    private String name="John";
+    private String classes="CO2";
+    public Student(){
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setClasses(String classes) {
+        this.classes = classes;
+    }
+    public String display(){
+        return name+" "+classes;
+    }
+}
---
+++ Accessmodifier_staticmethod_staticproperty/src/BT4/Test.java
@@ -0,0 +1,10 @@
+package BT4;
+
+public class Test {
+    public static void main(String[] args) {
+        Student student1=new Student();
+        student1.setName("Trung");
+        student1.setClasses("C0422i1");
+        System.out.println(student1.display());
+    }
+}
