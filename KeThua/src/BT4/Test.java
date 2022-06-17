2cb49ac25039488528861d6b01e88b3ca4fc42b6
NgoVietTrung <0611vietttrung@gmail.com>
2022-06-17 15:25:01 +0700

    update BT4

--- KeThua/src/BT4/Test.java
+++ KeThua/src/BT4/Test.java
@@ -4,7 +4,8 @@ import java.util.Arrays;
 
 public class Test {
     public static void main(String[] args) {
-
+        Point point=new Point(3,6);
+        System.out.println(point);
         MovablePoint movablePoint = new MovablePoint(3, 6, 1, 8);
         System.out.println(Arrays.toString(movablePoint.getXY()));
         System.out.println(movablePoint.move());
