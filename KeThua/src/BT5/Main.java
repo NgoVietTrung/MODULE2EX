7793601237ab398c08f031c01854e5d1b2a69a76
NgoVietTrung <0611vietttrung@gmail.com>
2022-06-20 14:24:48 +0700

    add BT5

---
+++ KeThua/src/BT5/Main.java
@@ -0,0 +1,8 @@
+package BT5;
+
+public class Main {
+    public static void main(String[] args) {
+        Triangle triangle=new Triangle(1,2,5,"blue",true);
+        System.out.println(triangle);
+    }
+}
