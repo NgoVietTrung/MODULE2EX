09873cac2bee656c73bc620e12ab115c5a261f38
NgoVietTrung <0611vietttrung@gmail.com>
2022-06-22 08:51:13 +0700

    add BT2

---
+++ Abstractandinterface/src/BT2/Circle.java
@@ -0,0 +1,53 @@
+package BT2;
+
+public class Circle {
+    double radius;
+    String color;
+    boolean filled=false;
+
+    public Circle() {
+    }
+
+    public Circle(double radius) {
+        this.radius = radius;
+    }
+
+    public Circle(double radius, String color) {
+        this.radius = radius;
+        this.color = color;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public boolean isFilled() {
+        return filled;
+    }
+
+    public void setFilled(boolean filled) {
+        this.filled = filled;
+    }
+
+    public void setRadius(double radius) {
+        this.radius = radius;
+    }
+
+    public double getRadius() {
+        return radius;
+    }
+
+    public double getArea() {
+        return radius * radius * Math.PI;
+    }
+
+    public String toString() {
+            return "A Circle with radius " + radius + " and color is " + color +" "+(filled?"filled":"not filled");
+
+    }
+}
+
