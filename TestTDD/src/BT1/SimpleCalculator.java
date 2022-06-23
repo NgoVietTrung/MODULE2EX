b0920491f50197bc10f8d57d83556854891ddfd5
NgoVietTrung <0611vietttrung@gmail.com>
2022-06-23 14:29:41 +0700

    add BT1

---
+++ TestTDD/src/BT1/SimpleCalculator.java
@@ -0,0 +1,10 @@
+package BT1;
+
+public class SimpleCalculator {
+    public static int add(int number1, int number2){
+        return number1+number2;
+    }
+    public static int sub(int number1, int number2){
+        return number1-number2;
+    }
+}
