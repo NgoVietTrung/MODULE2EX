3bf994f35a3cbf8e42074f8337a8c339b00bd8d3
NgoVietTrung <0611vietttrung@gmail.com>
2022-07-06 11:45:32 +0700

    add BT2

---
+++ BinaryFile/src/BT2/Main.java
@@ -0,0 +1,20 @@
+package BT2;
+
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.ObjectOutputStream;
+import java.util.List;
+
+public class Main {
+    public static void writeToFile(String path, List<Student> students) {
+        try {
+            FileOutputStream fos = new FileOutputStream(path);
+            ObjectOutputStream oos = new ObjectOutputStream(fos);
+            oos.writeObject(students);
+            oos.close();
+            fos.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
